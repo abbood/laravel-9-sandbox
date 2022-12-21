fa4129404aa189f93b52f2fe1a402a246626a957
Josh Harris <me@celant.co.uk>
2022-12-21 21:04:18 +0000

    Remove kra8/laravel-snowflake

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Kra8\Snowflake\HasSnowflakePrimary;
-use Kra8\Snowflake\Snowflake;
 
 class BaseModel extends Model
 {
-    use HasSnowflakePrimary;
-
     public $incrementing = false;
     protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            if (is_null($model->getKey())) {
+                $id = rand(1, 999999999999999);
+                $model->setAttribute($model->getKeyName(), $id);
+            }
+        });
+    }
 }
